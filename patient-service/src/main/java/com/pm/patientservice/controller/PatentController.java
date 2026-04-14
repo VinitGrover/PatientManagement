e9b841d4a7d523c3f99a799b9217fc37c57992cd
VinitGrover <vinitgrover255@gmail.com>
2026-04-15 01:53:01 +0530

    fine

---
+++ patient-service/src/main/java/com/pm/patientservice/controller/PatentController.java
@@ -0,0 +1,29 @@
+package com.pm.patientservice.controller;
+
+import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.service.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/patients")
+public class PatentController {
+    private final PatientService patientService;
+
+    @Autowired
+    public PatentController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PatientResponseDTO>> getPatients() {
+        List<PatientResponseDTO> patientResponseDTOS = patientService.getPatients();
+        return ResponseEntity.ok().body(patientResponseDTOS);
+    }
+
+}
